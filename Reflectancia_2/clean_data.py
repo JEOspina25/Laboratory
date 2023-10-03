1e92eda23ec27c8dd24ac9f74c8834953d9e9afd
JEOspina25 <juan.ospina25@udea.edu.co>
2023-10-02 19:04:44 -0500

    backup 2-oct

---
+++ Reflectancia_2/clean_data.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+class CleanData:
+    """Esta clase es para limpiar los datos que salen del espectrofotometro"""
+    
+    def __init__(self, path):
+        self.path = path
+
+    def read_data(self):
+        """Esta funcion lee los datos del espectrofotometro"""
+
+        path = self.path
+        # Read the data
+        data = pd.read_csv(path, skiprows = 14, sep = "\t")
+        data = data.drop(data.index[-1])
+        df = data.rename(columns={str(data.columns[0]): "Wavelength", str(data.columns[1]): "Intensity"})
+        name = str("clean_data/" + path.split("/")[-1] + ".csv")
+        df['Wavelength'] = df['Wavelength'].astype(float)
+        df['Intensity'] = df['Intensity'].astype(float)
+        df_filtrado = df[df['Wavelength'] <= 875]
+        df_filtrado = df[df['Wavelength'] >= 350]
+        df_filtrado.to_csv(name, index=False)
+        print("The data was cleaned and saved in: ", name)
+        return True
+
+
+    
+
+
+
+    def run(self):
+        """Esta funcion corre el codigo"""
+        self.read_data()
+        return True
